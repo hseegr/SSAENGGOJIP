7332425280c7cb8be1fa7d9eaa14fba3e53e483e
Riis <gunil099@gmail.com>
2025-03-25 15:02:02 +0900

    feat: 사이드바 아이콘 색상 변경, tailwindCSS 적용 1. 현재 선택된 아이콘 색상 파랑으로 표시하는 기능 추가 2. 기존 코드에서 tailwind CSS 적용하도록 수정

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,7 +1,7 @@
+
 /** @type {import('tailwindcss').Config} */
 import { defineConfig } from 'vite'
 import path from 'path'
-
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -9,6 +9,9 @@ export default {
       colors: {
         'ssaeng-purple': '#7171D7',
         'ssaeng-green': '#5195F7',
+        'ssaeng-gray-1': '#F4F4F4',
+        'ssaeng-gray-2': '#E5E5E5',
+        'ssaeng-gray-3': '#FCFCFC',
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         background: 'hsl(var(--background))',
