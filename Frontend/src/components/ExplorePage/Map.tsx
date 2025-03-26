847d446f25b30eeef848c1e5bc9861ddd5fc360f
Riis <gunil099@gmail.com>
2025-03-26 11:14:58 +0900

    feat: 지도 마커 표시, 클릭 시 중앙으로 두고 이동하는 애니메이션 추가 1. 카카오 지도 마커 표시(기초 주소는 멀티캠퍼스 역삼) 2. 마커 클릭 시 마커를 중심으로 이동하는 애니메이션 추가

--- Frontend/src/components/ExplorePage/Map.tsx
+++ Frontend/src/components/ExplorePage/Map.tsx
@@ -1,49 +1,59 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect } from 'react'
 
 declare global {
-    interface Window {
-        kakao: any;
-    }
+  interface Window {
+    kakao: any
+  }
 }
 
 const KakaoMap = () => {
-    const mapRef = useRef<HTMLDivElement>(null);
+  const mapRef = useRef<HTMLDivElement>(null)
 
-    useEffect(() => {
-        if (mapRef.current && window.kakao) {
-            const options = {
-                center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-                level: 3,
-            };
-            const map = new window.kakao.maps.Map(mapRef.current, options);
+  useEffect(() => {
+    if (mapRef.current && window.kakao) {
+      //   카카오맵 기본 설정
+      //   중심 : 멀티캠퍼스 역삼
+      //   줌 레벨 : 4
+      //   드래그 가능, 스크롤 가능
+      const options = {
+        center: new window.kakao.maps.LatLng(37.501317, 127.043502),
+        level: 4,
+        draggable: true,
+        scrollwheel: true,
+      }
+      // 첫 마커 위치
+      const markerPosition = new kakao.maps.LatLng(37.501317, 127.043502)
 
-            // 화면 크기가 변경될 때 지도를 재정렬
-            const handleResize = () => {
-                map.relayout();
-            };
+      // 지도 생성
+      const map = new window.kakao.maps.Map(mapRef.current, options)
+      // 마커를 생성합니다
+      const marker = new kakao.maps.Marker({
+        position: markerPosition,
+        clickable: true,
+      })
+      // 마커가 지도 위에 표시되도록 설정합니다
+      marker.setMap(map)
 
-            window.addEventListener("resize", handleResize);
-            return () => {
-                window.removeEventListener("resize", handleResize);
-            };
-        }
-    }, []);
+      // 마커 클릭 이벤트 추가
+      kakao.maps.event.addListener(marker, 'click', function () {
+        const TargetPosition = marker.getPosition()
+        map.panTo(TargetPosition)
+      })
+      // 화면 크기가 변경될 때 지도를 재정렬
+      const handleResize = () => {
+        map.relayout()
+      }
 
-    return (
-        <div
-            id="map"
-            ref={mapRef}
-            style={{
-                width: "100vw",
-                height: "100vh",
-                margin: "0",
-                padding: "0",
-                position: "relative",
-                top: "0",
-                left: "0",
-            }}
-        />
-    );
-};
+      window.addEventListener('resize', handleResize)
+      return () => {
+        window.removeEventListener('resize', handleResize)
+      }
+    }
+  }, [])
 
-export default KakaoMap;
+  return (
+    <div id="map" ref={mapRef} className="relative w-[95vw] h-screen m-0 p-0" />
+  )
+}
+
+export default KakaoMap
