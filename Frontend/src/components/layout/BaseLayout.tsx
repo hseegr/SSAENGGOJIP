b1c6213a0e4fa095e030edb2c8d896222e34a685
김희수 <hseegr@naver.com>
2025-03-26 15:02:02 +0900

    feat: 전체 ui 조정

--- Frontend/src/components/layout/BaseLayout.tsx
+++ Frontend/src/components/layout/BaseLayout.tsx
@@ -6,7 +6,7 @@ const BaseLayout = () => {
   return (
     <div className="flex flex-col w-full h-screen min-w-[1440px]">
       <Header />
-      <main className="flex-1">
+      <main className="flex-1 px-10">
         <Outlet />
       </main>
       <Footer />
