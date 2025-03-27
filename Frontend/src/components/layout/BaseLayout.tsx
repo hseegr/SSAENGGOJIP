313357d0497aee19c514cb2068d096bf76a6615a
hs <hseegr@naver.com>
2025-03-27 22:12:35 +0900

    feat: 지도 오류 수정

--- Frontend/src/components/layout/BaseLayout.tsx
+++ Frontend/src/components/layout/BaseLayout.tsx
@@ -6,7 +6,7 @@ const BaseLayout = () => {
   return (
     <div className="flex flex-col w-screen h-screen min-w-[1440px]">
       <Header />
-      <main className="flex-1 px-10">
+      <main className="flex-1">
         <Outlet />
       </main>
       <Footer />
