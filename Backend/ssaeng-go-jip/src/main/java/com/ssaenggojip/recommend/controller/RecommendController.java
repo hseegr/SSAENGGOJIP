ce5102085780287df05c26d4390c0afcdc6ec7fc
송정호 <dynamite885@naver.com>
2025-03-31 16:37:16 +0900

    refactor: 테스트 API 제거 및 엔드포인트 수정

--- Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/recommend/controller/RecommendController.java
+++ Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/recommend/controller/RecommendController.java
@@ -2,20 +2,16 @@ package com.ssaenggojip.recommend.controller;
 
 import com.ssaenggojip.apiPayload.ApiResponse;
 import com.ssaenggojip.auth.annotation.AuthUser;
-import com.ssaenggojip.common.enums.SocialLoginType;
+import com.ssaenggojip.recommend.dto.request.RecommendByPreferencesRequest;
 import com.ssaenggojip.recommend.dto.response.FacilityPreferencesResponse;
 import com.ssaenggojip.recommend.dto.request.RecommendByLocationRequest;
 import com.ssaenggojip.recommend.dto.request.UpdateFacilityPreferencesRequest;
 import com.ssaenggojip.recommend.dto.response.RecommendPropertyListResponse;
 import com.ssaenggojip.recommend.service.RecommendService;
 import com.ssaenggojip.user.entity.User;
-import com.ssaenggojip.user.repository.UserRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,7 +20,9 @@ public class RecommendController {
     private final RecommendService recommendService;
 
     @GetMapping("/facility-preferences")
-    public ApiResponse<FacilityPreferencesResponse> getFacilityPreferences(@AuthUser User user) {
+    public ApiResponse<FacilityPreferencesResponse> getFacilityPreferences(
+            @AuthUser User user
+    ) {
         return ApiResponse.onSuccess(recommendService.getPreferences(user));
     }
 
@@ -37,52 +35,15 @@ public class RecommendController {
         return ApiResponse.onSuccess(null);
     }
 
-    private final UserRepository userRepository;
-    @GetMapping("/test")
-    public ApiResponse<Void> test() {
-        List<Double> vec1 = new ArrayList<>();
-        vec1.add(0.1);
-        vec1.add(0.1);
-        vec1.add(0.1);
-        vec1.add(0.1);
-        vec1.add(0.1);
-        vec1.add(0.1);
-        vec1.add(0.1);
-        vec1.add(0.1);
-        List<Double> vec2 = new ArrayList<>();
-        vec2.add(0.2);
-        vec2.add(0.2);
-        vec2.add(0.2);
-        vec2.add(0.2);
-        vec2.add(0.2);
-        vec2.add(0.2);
-        vec2.add(0.2);
-        vec2.add(0.2);
-
-        User user = User.builder()
-                .nickname("test")
-                .email("test@example.com")
-                .emailVerified(true)
-                .socialLoginId("test")
-                .socialLoginType(SocialLoginType.GOOGLE)
-                .build();
-        user.setFacilityPreferences(vec1);
-        userRepository.save(user);
-        System.out.println(recommendService.getPreferences(user).toString());
-        user.setFacilityPreferences(vec2);
-        userRepository.save(user);
-        System.out.println(recommendService.getPreferences(user).toString());
-        return ApiResponse.onSuccess(null);
+    @PostMapping("/by-preferences")
+    public ApiResponse<RecommendPropertyListResponse> recommendByPreferences(
+            @RequestBody @Valid RecommendByPreferencesRequest request,
+            @AuthUser User user
+    ) {
+        return ApiResponse.onSuccess(recommendService.findTopKByPreferences(user, request.getK()));
     }
 
-    @GetMapping("/topk")
-    public ApiResponse<Void> topk() {
-        User user = userRepository.getReferenceById(1L);
-        recommendService.findTopKByPreferences(user, 10);
-        return ApiResponse.onSuccess(null);
-    }
-
-    @GetMapping("/locations")
+    @PostMapping("/by-locations")
     public ApiResponse<RecommendPropertyListResponse> recommendByLocation(
             @RequestBody @Valid RecommendByLocationRequest request
     ) {
