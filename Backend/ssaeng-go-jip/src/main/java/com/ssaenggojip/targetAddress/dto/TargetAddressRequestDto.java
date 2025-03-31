4ac7e32bb9aea050f35ad2c930e5867b844ee4d5
kimjimin <kimjm9841@naver.com>
2025-03-31 11:07:35 +0900

    feat: 타겟 주소 요청값 유효성 검사

--- Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/targetAddress/dto/TargetAddressRequestDto.java
+++ Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/targetAddress/dto/TargetAddressRequestDto.java
@@ -1,17 +1,38 @@
 package com.ssaenggojip.targetAddress.dto;
 
 import com.ssaenggojip.common.enums.TransportMode;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 
 @Getter
 public class TargetAddressRequestDto {
+
+    @NotBlank
+    @Size(max = 200)
     String address;
+
+    @NotBlank
+    @Size(max = 10)
     String name;
+
+    @NotNull
+    @Digits(integer = 3, fraction = 6)
     BigDecimal latitude;
+
+    @NotNull
+    @Digits(integer = 3, fraction = 6)
     BigDecimal longitude;
+
+    @NotNull
     TransportMode transportMode;
+
+    @NotNull
+    @Min(0)
     Integer travelTime;
+
+    @NotNull
+    @Min(0)
     Integer walkTime;
 }
