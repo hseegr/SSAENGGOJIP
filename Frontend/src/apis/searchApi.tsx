4b128d11038801703a8e4ce62de93d908d085255
Riis <gunil099@gmail.com>
2025-03-31 13:54:12 +0900

    feat: 일반 검색 검색 기능 추가 1. 필터와 연계되는 검색 기능 추가 문제점 - 실제 API와 연결이 되어있지 않음 - 기초 데이터를 불러오는 코드 필요 + 검색해서 가져온 코드를 가격 순으로 필터링 하는 코드 필요 - 타입 오류들 수정 필요

---
+++ Frontend/src/apis/searchApi.tsx
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+// 검색 API 요청 함수
+export const fetchSearchResults = async (
+  query: string,
+  filters: Record<string, []>,
+) => {
+  try {
+    const response = await axios.get('/api/search', {
+      params: {
+        query,
+        ...filters, // 필터 데이터를 병합
+      }, // 검색어를 쿼리 파라미터로 전달
+    })
+    return response.data // 응답 데이터를 반환
+  } catch (error) {
+    console.error('검색 API 요청 중 오류 발생:', error)
+    throw error // 에러를 호출한 곳으로 전달
+  }
+}
