5ce9c6e950674d285de14538ba73cb5394bf0e44
김희수 <hseegr@naver.com>
2025-04-01 10:39:56 +0900

    feat: 메인페이지 api연결 및 에러 메시지 처리 완료

--- Frontend/src/components/layout/BaseLayout.tsx
+++ Frontend/src/components/layout/BaseLayout.tsx
@@ -2,6 +2,8 @@
 import { Outlet, useLocation } from 'react-router-dom'
 import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
+import { ToastContainer } from 'react-toastify' // 토스트 메시지 추가
+import 'react-toastify/dist/ReactToastify.css' // 스타일도 추가
 
 const BaseLayout = () => {
   const location = useLocation()
@@ -15,6 +17,16 @@ const BaseLayout = () => {
         <Outlet />
       </main>
       {!shouldHideFooter && <Footer />}
+      {/* 토스트 에러 처리 메시지를 위함 */}
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="light"
+      />
     </div>
   )
 }
