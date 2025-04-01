e06b3f8601b8bdcafda80a6df615fe47a2d4658b
송정호 <dynamite885@naver.com>
2025-04-01 13:15:59 +0900

    feat: 주변 시설 선호도 response에 타입 리스트를 포함하도록 수정

--- Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/recommend/dto/response/FacilityPreferencesResponse.java
+++ Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/recommend/dto/response/FacilityPreferencesResponse.java
@@ -10,5 +10,16 @@ import java.util.List;
 @Builder
 @ToString
 public class FacilityPreferencesResponse {
+    private List<String> facilityTypeList;
     private List<Double> facilityPreferences;
+
+    public static FacilityPreferencesResponse from(
+            List<String> facilityTypeList,
+            List<Double> facilityPreferences
+    ) {
+        return FacilityPreferencesResponse.builder()
+                .facilityTypeList(facilityTypeList)
+                .facilityPreferences(facilityPreferences)
+                .build();
+    }
 }
