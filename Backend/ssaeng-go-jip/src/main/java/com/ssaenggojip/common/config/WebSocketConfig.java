5f10e468b640e373ff3e445120bc7507ef4c8f4f
kimjimin <kimjm9841@naver.com>
2025-04-02 15:35:35 +0900

    feat: SockJS 제거

--- Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/common/config/WebSocketConfig.java
+++ Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/common/config/WebSocketConfig.java
@@ -25,7 +25,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
                 .addInterceptors(jwtHandshakeInterceptor)
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*");
     }
 }
