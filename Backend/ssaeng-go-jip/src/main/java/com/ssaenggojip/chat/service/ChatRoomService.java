e1eecf417456a73ef17cfb3e09bcb31471da8c27
kimjimin <kimjm9841@naver.com>
2025-04-02 10:46:56 +0900

    feat: 채팅방 입장, 퇴장 REST API로 변경

--- Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/chat/service/ChatRoomService.java
+++ Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/chat/service/ChatRoomService.java
@@ -1,14 +1,21 @@
 package com.ssaenggojip.chat.service;
 
+import com.mongodb.client.result.UpdateResult;
 import com.ssaenggojip.chat.converter.ChatConverter;
 import com.ssaenggojip.chat.dto.ChatRoomResponseDto;
+import com.ssaenggojip.chat.entity.ChatRoom;
 import com.ssaenggojip.chat.entity.UserChatRoom;
 import com.ssaenggojip.chat.repository.ChatRoomRepository;
 import com.ssaenggojip.chat.repository.UserChatRoomRepository;
 import com.ssaenggojip.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,6 +26,8 @@ public class ChatRoomService {
 
     private final ChatRoomRepository chatRoomRepository;
     private final UserChatRoomRepository userChatRoomRepository;
+
+    private final MongoTemplate mongoTemplate;
 
     public List<ChatRoomResponseDto> getPopularChatRoom() {
         return chatRoomRepository.findTop10ByOrderByUserCountDesc()
@@ -36,5 +45,43 @@ public class ChatRoomService {
 
         return chatRoomRepository.findByIdIn(chatRoomIdList)
                 .stream().map(ChatConverter::toChatRoomResponseDto).toList();
+    }
+
+    @Transactional
+    public void enterChatRoom(User user, String chatRoomId) {
+        if (userChatRoomRepository.findByUserAndChatRoomId(user, chatRoomId).isPresent()) {
+            return;
+        }
+
+        Query query = new Query(
+                Criteria.where("_id").is(chatRoomId)
+                        .and("userCount").lt(1000)
+        );
+
+        Update update = new Update().inc("userCount", 1);
+        UpdateResult result = mongoTemplate.updateFirst(query, update, ChatRoom.class);
+
+        if (result.getModifiedCount() == 0) {
+            throw new IllegalStateException("채팅방 인원이 가득 찼습니다.");
+        }
+
+        userChatRoomRepository.save(UserChatRoom.builder()
+                .chatRoomId(chatRoomId)
+                .user(user)
+                .build());
+    }
+
+    @Transactional
+    public void leaveChatRoom(User user, String chatRoomId) {
+        if (userChatRoomRepository.findByUserAndChatRoomId(user, chatRoomId).isEmpty()) {
+            return;
+        }
+
+        userChatRoomRepository.deleteByUserAndChatRoomId(user, chatRoomId);
+
+        Query query = new Query(Criteria.where("_id").is(chatRoomId));
+        Update update = new Update().inc("userCount", -1);
+
+        mongoTemplate.updateFirst(query, update, ChatRoom.class);
     }
 }
