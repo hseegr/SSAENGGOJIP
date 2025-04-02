a817219c2098b6910288317839c3a1c31131416a
kimjimin <kimjm9841@naver.com>
2025-04-02 16:49:15 +0900

    feat: 스웨거에서 쿠키 삭제

--- Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/common/config/SwaggerConfig.java
+++ Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/common/config/SwaggerConfig.java
@@ -1,23 +1,20 @@
 package com.ssaenggojip.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @OpenAPIDefinition(
         info = @Info(
                 title = "쌩고집 API",
                 version = "v1",
                 description = "쌩고집 API 문서입니다."
         ),
-        security = {
-                @SecurityRequirement(name = "bearerAuth"),
-                @SecurityRequirement(name = "refreshTokenCookie")
-        }
+        security = @SecurityRequirement(name = "bearerAuth")
 )
 @SecurityScheme(
         name = "bearerAuth",
@@ -25,12 +22,5 @@ import org.springframework.context.annotation.Configuration;
         scheme = "bearer",
         bearerFormat = "JWT"
 )
-@SecurityScheme(
-        name = "refreshTokenCookie",
-        type = SecuritySchemeType.APIKEY,
-        in = SecuritySchemeIn.COOKIE,
-        paramName = "refresh-token"
-)
-@Configuration
 public class SwaggerConfig {
 }
