a9bd4e3af816243b8b9f06c31816470d8c7288e8
eyeben <qkrehdals97@naver.com>
2025-04-04 16:31:46 +0900

    Fix: 스웨거 문서화 반영되도록 수정
    스웨거 문서화 반영되도록 수정

--- Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/property/controller/PropertyController.java
+++ Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/property/controller/PropertyController.java
@@ -3,12 +3,19 @@ package com.ssaenggojip.property.controller;
 
 import com.ssaenggojip.apipayload.ApiResponse;
 import com.ssaenggojip.property.dto.request.CoordinateGetRequest;
+import com.ssaenggojip.property.dto.request.RecommendSearchRequest;
 import com.ssaenggojip.property.dto.request.SearchRequest;
 import com.ssaenggojip.property.dto.request.TransportTimeRequest;
+import com.ssaenggojip.property.dto.response.CoordinateResponse;
+import com.ssaenggojip.property.dto.response.DetailResponse;
+import com.ssaenggojip.property.dto.response.SearchResponse;
+import com.ssaenggojip.property.dto.response.TransportTimeResponse;
 import com.ssaenggojip.property.service.PropertyFacade;
 import com.ssaenggojip.property.service.PropertyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/properties")
@@ -20,29 +27,29 @@ public class PropertyController {
 
 
     @PostMapping("/search")
-    public ApiResponse<?> searchProperty(@RequestBody SearchRequest searchRequest) {
+    public ApiResponse<SearchResponse> searchProperty(@RequestBody SearchRequest searchRequest) {
         return ApiResponse.onSuccess(propertyFacade.searchProperties(searchRequest));
     }
 
     @GetMapping("/{propertyId}")
-    public ApiResponse<?> getPropertyDetail(@PathVariable Long propertyId) {
+    public ApiResponse<DetailResponse> getPropertyDetail(@PathVariable Long propertyId) {
         return ApiResponse.onSuccess(propertyFacade.getDetail(propertyId));
     }
 
     @PostMapping("/get-transport-time")
-    public ApiResponse<?> getTransportTime(@RequestBody TransportTimeRequest transportTimeRequest) {
+    public ApiResponse<TransportTimeResponse> getTransportTime(@RequestBody TransportTimeRequest transportTimeRequest) {
         return ApiResponse.onSuccess(propertyFacade.getTransportTime(transportTimeRequest));
     }
 
     @PostMapping("/coordinates")
-    public ApiResponse<?> getCoordinates(@RequestBody CoordinateGetRequest coordinateGetRequest) {
+    public ApiResponse<List<CoordinateResponse>> getCoordinates(@RequestBody CoordinateGetRequest coordinateGetRequest) {
         return ApiResponse.onSuccess(propertyService.getCoordinates(coordinateGetRequest));
     }
 
-//    @PostMapping("/recommend-search")
-//    public ApiResponse<?> searchRecommend(RecommendSearchRequest recommendSearchRequest) {
-//        return null;
-//    }
+    @PostMapping("/recommend-search")
+    public ApiResponse<?> searchRecommend(RecommendSearchRequest recommendSearchRequest) {
+        return null;
+    }
 //
 //    @PostMapping("/recommend-detail")
 //    public ApiResponse<?> getRecommendDetail(RecommendDetailRequest recommendDetailRequest) {
