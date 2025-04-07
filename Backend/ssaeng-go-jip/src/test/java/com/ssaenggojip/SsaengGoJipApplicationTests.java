4e2a3dc8c6da5868c4ebbe77644ea00a340a4d27
kimjimin <kimjm9841@naver.com>
2025-04-07 12:17:43 +0900

    test: 덮어쓰기 허용 설정

--- Backend/ssaeng-go-jip/src/test/java/com/ssaenggojip/SsaengGoJipApplicationTests.java
+++ Backend/ssaeng-go-jip/src/test/java/com/ssaenggojip/SsaengGoJipApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.main.allow-bean-definition-overriding=true")
 @Import(TestRedisConfig.class)
 class SsaengGoJipApplicationTests {
 
