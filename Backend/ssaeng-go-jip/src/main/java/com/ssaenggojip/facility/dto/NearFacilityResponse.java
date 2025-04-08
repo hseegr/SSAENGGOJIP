c0d4b2c24d186f1e1dbe562975d1967477b83e15
송정호 <dynamite885@naver.com>
2025-04-08 16:53:08 +0900

    feat: 응답에 시설 이름 추가

--- Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/facility/dto/NearFacilityResponse.java
+++ Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/facility/dto/NearFacilityResponse.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 public class NearFacilityResponse {
     private Long id;
     private Long facilityTypeId;
+    private Long facilityTypeName;
     private String name;
     private String address;
     private Double latitude;
