a02a4636b0d05552b65d72cfa2c3b3c09766f724
eyeben <62013201+eyeben@users.noreply.github.com>
2025-04-10 00:07:19 +0900

    Refactor: 쿼리 최적화
    공간 인덱스 쿼리 최적화

--- Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/property/repository/PropertyRepository.java
+++ Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/property/repository/PropertyRepository.java
@@ -11,42 +11,79 @@ import java.util.List;
 
 public interface PropertyRepository extends JpaRepository<Property, Long> {
 
-    @Query(value = """
-    SELECT * FROM property p
-    WHERE (:dealType IS NULL OR p.deal_type::text = :dealType)
-      AND (:propertyTypesEmpty = true OR p.property_type::text IN (:propertyTypes))
-      AND p.price BETWEEN :minPrice AND :maxPrice
-      AND p.rent_price BETWEEN :minRentPrice AND :maxRentPrice
-      AND (
-        (:isStationSearch = true AND
-         ST_DWithin(
-            ST_Transform(p.geom, 3857),
-            ST_Transform(
-              ST_SetSRID(ST_MakePoint(:lng, :lat), 4326),
-              3857
-            ),
-            :dist
-         )
-        ) OR
-        (:isStationSearch = false AND (
-            :search IS NULL OR p.address LIKE :search OR p.name LIKE :search
-        ))
-      )
-    """, nativeQuery = true)
-    List<Property> searchFilteredProperties(
-            @Param("dealType") String dealType,
-            @Param("propertyTypes") List<String> propertyTypes,
-            @Param("propertyTypesEmpty") boolean propertyTypesEmpty,
-            @Param("minPrice") Long minPrice,
-            @Param("maxPrice") Long maxPrice,
-            @Param("minRentPrice") Long minRentPrice,
-            @Param("maxRentPrice") Long maxRentPrice,
-            @Param("search") String search,
-            @Param("lng") BigDecimal lng,
-            @Param("lat") BigDecimal lat,
-            @Param("isStationSearch") boolean isStationSearch,
-            @Param("dist") Integer dist
-    );
+    // ** 성능 테스트 용으로 남겨둘 것
+//    @Query(value = """
+//    SELECT * FROM property p
+//    WHERE (:dealType IS NULL OR p.deal_type::text = :dealType)
+//      AND (:propertyTypesEmpty = true OR p.property_type::text IN (:propertyTypes))
+//      AND p.price BETWEEN :minPrice AND :maxPrice
+//      AND p.rent_price BETWEEN :minRentPrice AND :maxRentPrice
+//      AND (
+//        (:isStationSearch = true AND
+//         ST_DWithin(
+//            ST_Transform(p.geom, 3857),
+//            ST_Transform(
+//              ST_SetSRID(ST_MakePoint(:lng, :lat), 4326),
+//              3857
+//            ),
+//            :dist
+//         )
+//        ) OR
+//        (:isStationSearch = false AND (
+//            :search IS NULL OR p.address LIKE :search OR p.name LIKE :search
+//        ))
+//      )
+//    """, nativeQuery = true)
+//    List<Property> searchFilteredProperties(
+//            @Param("dealType") String dealType,
+//            @Param("propertyTypes") List<String> propertyTypes,
+//            @Param("propertyTypesEmpty") boolean propertyTypesEmpty,
+//            @Param("minPrice") Long minPrice,
+//            @Param("maxPrice") Long maxPrice,
+//            @Param("minRentPrice") Long minRentPrice,
+//            @Param("maxRentPrice") Long maxRentPrice,
+//            @Param("search") String search,
+//            @Param("lng") BigDecimal lng,
+//            @Param("lat") BigDecimal lat,
+//            @Param("isStationSearch") boolean isStationSearch,
+//            @Param("dist") Integer dist
+//    );
+@Query(value = """
+SELECT * FROM property p
+WHERE (:dealType IS NULL OR p.deal_type::text = :dealType)
+  AND (:propertyTypesEmpty = true OR p.property_type::text IN (:propertyTypes))
+  AND p.price BETWEEN :minPrice AND :maxPrice
+  AND p.rent_price BETWEEN :minRentPrice AND :maxRentPrice
+  AND (
+    (:isStationSearch = true AND
+     ST_DWithin(
+        p.geom_3857,
+        ST_Transform(
+          ST_SetSRID(ST_MakePoint(:lng, :lat), 4326),
+          3857
+        ),
+        :dist
+     )
+    ) OR
+    (:isStationSearch = false AND (
+        :search IS NULL OR p.address LIKE :search OR p.name LIKE :search
+    ))
+  )
+""", nativeQuery = true)
+List<Property> searchFilteredProperties(
+        @Param("dealType") String dealType,
+        @Param("propertyTypes") List<String> propertyTypes,
+        @Param("propertyTypesEmpty") boolean propertyTypesEmpty,
+        @Param("minPrice") Long minPrice,
+        @Param("maxPrice") Long maxPrice,
+        @Param("minRentPrice") Long minRentPrice,
+        @Param("maxRentPrice") Long maxRentPrice,
+        @Param("search") String search,
+        @Param("lng") BigDecimal lng,
+        @Param("lat") BigDecimal lat,
+        @Param("isStationSearch") boolean isStationSearch,
+        @Param("dist") Integer dist
+);
 
 
 
@@ -111,63 +148,63 @@ public interface PropertyRepository extends JpaRepository<Property, Long> {
     );
 
     @Query(value = """
-    WITH user_point AS (
-        SELECT ST_SetSRID(ST_MakePoint(:lng, :lat), 4326) AS geom
-    ),
-    station_a AS (
-        SELECT
-            s.id AS station_a_id,
-            ST_DistanceSphere(ST_MakePoint(s.longitude, s.latitude), up.geom) / 80 AS t1
-        FROM station s, user_point up
-        WHERE ST_DistanceSphere(ST_MakePoint(s.longitude, s.latitude), up.geom) <= :walkTime * 80
-    ),
-    station_b AS (
-        SELECT
-            sm.destination_station_id AS station_b_id,
-            sa.station_a_id,
-            sa.t1,
-            sm.transport_time AS t2
-        FROM station_route sm
-        JOIN station_a sa ON sm.departure_station_id = sa.station_a_id
-        WHERE sm.transport_time <= (:totalTime - sa.t1)
-    ),
-    property_candidates AS (
-        SELECT
-            sb.station_b_id,
-            sb.station_a_id,
-            sb.t1,
-            sb.t2,
-            ns.property_id,
-            ns.walk_time AS t3,
-            (sb.t1 + sb.t2 + ns.walk_time) AS total_time
-        FROM station_b sb
-        JOIN near_station ns ON sb.station_b_id = ns.station_id
-        WHERE ns.walk_time <= (:walkTime - sb.t1)
-    )
+WITH user_point AS (
+    SELECT ST_Transform(ST_SetSRID(ST_MakePoint(:lng, :lat), 4326), 3857) AS geom_3857
+),
+station_a AS (
     SELECT
-        p.id,
-        false AS is_recommend,
-        p.deal_type,
-        p.price,
-        p.rent_price,
-        p.maintenance_price,
-        p.total_floor,
-        p.floor,
-        p.exclusive_area AS area,
-        p.address,
-        p.latitude,
-        p.longitude,
-        false AS is_interest,
-        p.main_image AS image_url,
-        CAST(pc.total_time AS INTEGER)
-    FROM property_candidates pc
-    JOIN property p ON pc.property_id = p.id
-    WHERE (:dealType IS NULL OR p.deal_type::text = :dealType)
-      AND (:propertyTypesEmpty OR p.property_type::text IN (:propertyTypes))
-      AND p.price BETWEEN :minPrice AND :maxPrice
-      AND p.rent_price BETWEEN :minRentPrice AND :maxRentPrice
-      AND pc.total_time <= :totalTime
-    ORDER BY pc.total_time ASC
+        s.id AS station_a_id,
+        ST_Distance(s.geom_3857, up.geom_3857) / 80 AS t1
+    FROM station s, user_point up
+    WHERE ST_DWithin(s.geom_3857, up.geom_3857, :walkTime * 80)
+),
+station_b AS (
+    SELECT
+        sm.destination_station_id AS station_b_id,
+        sa.station_a_id,
+        sa.t1,
+        sm.transport_time AS t2
+    FROM station_route sm
+    JOIN station_a sa ON sm.departure_station_id = sa.station_a_id
+    WHERE sm.transport_time <= (:totalTime - sa.t1)
+),
+property_candidates AS (
+    SELECT
+        sb.station_b_id,
+        sb.station_a_id,
+        sb.t1,
+        sb.t2,
+        ns.property_id,
+        ns.walk_time AS t3,
+        (sb.t1 + sb.t2 + ns.walk_time) AS total_time
+    FROM station_b sb
+    JOIN near_station ns ON sb.station_b_id = ns.station_id
+    WHERE ns.walk_time <= (:walkTime - sb.t1)
+)
+SELECT
+    p.id,
+    false AS is_recommend,
+    p.deal_type,
+    p.price,
+    p.rent_price,
+    p.maintenance_price,
+    p.total_floor,
+    p.floor,
+    p.exclusive_area AS area,
+    p.address,
+    p.latitude,
+    p.longitude,
+    false AS is_interest,
+    p.main_image AS image_url,
+    CAST(pc.total_time AS INTEGER)
+FROM property_candidates pc
+JOIN property p ON pc.property_id = p.id
+WHERE (:dealType IS NULL OR p.deal_type::text = :dealType)
+  AND (:propertyTypesEmpty OR p.property_type::text IN (:propertyTypes))
+  AND p.price BETWEEN :minPrice AND :maxPrice
+  AND p.rent_price BETWEEN :minRentPrice AND :maxRentPrice
+  AND pc.total_time <= :totalTime
+ORDER BY pc.total_time ASC
 """, nativeQuery = true)
     List<RecommendSearchDto> findReachableProperties(
             @Param("lng") Double lng,
@@ -182,6 +219,7 @@ public interface PropertyRepository extends JpaRepository<Property, Long> {
             @Param("minRentPrice") Long minRentPrice,
             @Param("maxRentPrice") Long maxRentPrice
     );
+
 
 
 
