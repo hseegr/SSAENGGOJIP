17f10c31fa6c57f51297f810384e5052d933f85d
송정호 <dynamite885@naver.com>
2025-04-10 12:41:29 +0900

    perf: 쿼리 성능 개선

--- Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/property/repository/PropertyRepository.java
+++ Backend/ssaeng-go-jip/src/main/java/com/ssaenggojip/property/repository/PropertyRepository.java
@@ -80,16 +80,10 @@ public interface PropertyRepository extends JpaRepository<Property, Long> {
     @Query(nativeQuery = true,
             value = "SELECT * " +
                     "FROM property " +
-                    "WHERE ST_DWithin(" +
-                    "geography(ST_SetSRID(ST_Point(longitude, latitude), 4326)), " +
-                    "geography(ST_SetSRID(ST_Point(:lng, :lat), 4326)), " +
-                    ":radius) " +
-                    "ORDER BY ST_Distance(" +
-                    "   geom, " +
-                    "   geography(" +
-                    "       ST_SetSRID(ST_Point(:lng, :lat), 4326)" +
-                    "   )" +
-                    ") ASC " +
+                    "WHERE " +
+                    "   geom <-> geography(ST_Point(127.1, 37.5)) <= 1000 " +
+                    "ORDER BY " +
+                    "   geom <-> geography(ST_Point(127.1, 37.5)) ASC " +
                     "LIMIT 100 "
     )
     List<Property> findByLocation(
