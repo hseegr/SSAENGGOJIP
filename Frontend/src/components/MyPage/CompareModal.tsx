314a0dada3d22fa0232aae7a5c4f12e8a65f93a8
TaeYeongKIM <taeyeong0114@gmail.com>
2025-04-11 02:13:34 +0900

    fix: 관심매물 비교 오류 수정

--- Frontend/src/components/MyPage/CompareModal.tsx
+++ Frontend/src/components/MyPage/CompareModal.tsx
@@ -1,16 +1,16 @@
 import { X } from 'lucide-react'
 import PropertyDetail from '@/components/common/property/PropertyDetail'
+import { LikedProperty } from '@/services/propertyService'
 
 type Props = {
-    selectedIds: number[]
+    selectedProperties: LikedProperty[]
     onClose: () => void
 }
 
-const CompareModal = ({ selectedIds, onClose }: Props) => {
+const CompareModal = ({ selectedProperties, onClose }: Props) => {
     return (
         <div className="fixed inset-0 bg-black/50 z-50 flex items-center justify-center p-4">
             <div className="bg-white w-full max-w-[1200px] max-h-[90vh] rounded-xl relative flex flex-col overflow-hidden">
-                {/* 닫기 버튼 */}
                 <button
                     onClick={onClose}
                     className="absolute top-6 right-6 text-gray-500 hover:text-gray-700 z-10"
@@ -18,22 +18,27 @@ const CompareModal = ({ selectedIds, onClose }: Props) => {
                     <X size={24} />
                 </button>
 
-                {/* 타이틀 */}
                 <h3 className="mt-10 mb-6 text-xl font-bold text-ssaeng-purple text-center">
                     관심 매물 비교하기
                 </h3>
 
-                {/* 스크롤이 적용되는 내부 영역 */}
                 <div className="overflow-y-auto px-6 pb-6">
                     <div
-                        className={`grid gap-6 ${selectedIds.length === 2 ? 'grid-cols-2' : 'grid-cols-3'}`}
+                        className={`grid gap-6 ${selectedProperties.length === 2 ? 'grid-cols-2' : 'grid-cols-3'
+                            }`}
                     >
-                        {selectedIds.map((id) => (
+                        {selectedProperties.map((property) => (
                             <div
-                                key={id}
+                                key={property.id}
                                 className="border rounded-xl shadow-sm bg-white"
                             >
-                                <PropertyDetail id={id} onClose={() => { }} isCompareMode />
+                                <PropertyDetail
+                                    id={property.id}
+                                    latitude={property.latitude}
+                                    longitude={property.longitude}
+                                    onClose={() => { }}
+                                    isCompareMode
+                                />
                             </div>
                         ))}
                     </div>
